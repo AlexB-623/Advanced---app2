845e7aa72e12c03d713f947e2663e84cd8c9e858
alexb <alex.brown577@gmail.com>
2024-11-25 11:14:34 -0500

    Day 2

--- main.py
+++ main.py
@@ -1,75 +1,47 @@
-from fpdf import FPDF
+from flat import Bill, Flatmate
+from report import PdfReport
 
 
-class Bill:
+def dih_validator(who):
     """
-    Creates an object of Bill class, contains data about an expense (amount and period)
+    This function validates that days-in-house is an integer
+    :param who: flatmate name variable
+    :return: integer for days-in-house
     """
-
-    def __init__(self, amount, period):
-        self.amount = amount
-        self.period = period
-
-
-class Flatmate:
-    """
-    Creates and object of a Flatmate class,
-    contains data about the flatmate and amount of time they spent in the home
-    """
-
-    def __init__(self, name, days_in_house):
-        self.name = name
-        self.days_in_house = days_in_house
+    while True:
+        user_input = input(f"How many days in the bill period was {who} in the house? ")
+        try:
+            dih = int(user_input)
+            break
+        except ValueError:
+            print("Please only enter whole numbers, i.e. 25 30 or 16")
+    return dih
 
 
-    def pays(self, bill, flatmate2):
-        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
-        to_pay = bill.amount * weight
-        return to_pay
+#while loop to validate bill amount input:
+while True:
+    bill_input = input("Please enter the bill amount: ")
+    try:
+        bill_amount = float(bill_input)
+        break
+    except ValueError:
+        print("Please enter only numeric values in the following format: 000.00")
+
+bill_period = input("Please enter the bill's date period: ")
+flat_mate_1 = input("Please enter your name: ")
+#while loop to validate days input
+fm1_dih = dih_validator(flat_mate_1)
+
+flat_mate_2 = input("Please enter the other flatmate's name: ")
+fm2_dih = dih_validator(flat_mate_2)
 
 
-class PdfReport:
-    """
-    Creates a PDF with data about the bill & the split.
-    """
+the_bill = Bill(amount=bill_amount, period=bill_period)
+fm1 = Flatmate(name=flat_mate_1, days_in_house=fm1_dih)
+fm2 = Flatmate(name=flat_mate_2, days_in_house=fm2_dih)
 
-    def __init__(self, filename):
-        self.filename = filename
+# print(f'John pays: {john.pays(bill=the_bill, flatmate2=marry)}')
+# print(f'Marry pays: {marry.pays(bill=the_bill, flatmate2=john)}')
 
-    def generate(self, flatmate1, flatmate2, bill):
-        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
-        pdf = FPDF(orientation='P', unit='pt', format='A4')
-        pdf.add_page()
-
-        #add icon
-        pdf.image(r'C:\Users\alexb\PycharmProjects\App-2-Flatmates-Bill\files\house.png')
-
-        # insert title
-        pdf.set_font('Times', 'B', 24)
-        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align='C', ln=1)
-
-        #insert period label/value
-        pdf.cell(w=80, h=40, txt="Period:", border=1)
-        pdf.cell(w=175, h=40, txt=bill.period, border=1, ln=1)
-
-        # insert name and amount due for flatmate1
-        pdf.cell(w=80, h=40, txt=flatmate1.name, border=1)
-        pdf.cell(w=175, h=40, txt=flatmate1_pay, border=1, ln=1)
-
-        # insert name and amount due for flatmate2
-        pdf.cell(w=80, h=40, txt=flatmate2.name, border=1)
-        pdf.cell(w=175, h=40, txt=flatmate2_pay, border=1, ln=1)
-
-        pdf.output(self.filename)
-
-
-the_bill = Bill(amount=120, period="October 2024")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
-
-print(f'John pays: {john.pays(bill=the_bill, flatmate2=marry)}')
-print(f'Marry pays: {marry.pays(bill=the_bill, flatmate2=john)}')
-
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
+pdf_report = PdfReport(filename=f"{bill_period}.pdf")
+pdf_report.generate(flatmate1=fm1, flatmate2=fm2, bill=the_bill)
---
+++ report.py
@@ -0,0 +1,43 @@
+import webbrowser, os
+from fpdf import FPDF
+
+
+class PdfReport:
+    """
+    Creates a PDF with data about the bill & the split.
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        #add icon
+        pdf.image(r'files/house.png')
+
+        # insert title
+        pdf.set_font('Times', 'B', 24)
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=1, align='C', ln=1)
+
+        #insert period label/value
+        pdf.set_font('Times', 'B', 14)
+        pdf.cell(w=80, h=40, txt="Period:", border=1)
+        pdf.cell(w=175, h=40, txt=bill.period, border=1, ln=1)
+
+        # insert name and amount due for flatmate1
+        pdf.set_font('Times', size=12)
+        pdf.cell(w=80, h=40, txt=flatmate1.name, border=1)
+        pdf.cell(w=175, h=40, txt=flatmate1_pay, border=1, ln=1)
+
+        # insert name and amount due for flatmate2
+        pdf.set_font('Times', size=12)
+        pdf.cell(w=80, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=175, h=40, txt=flatmate2_pay, border=1, ln=1)
+
+        os.chdir("files")
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
